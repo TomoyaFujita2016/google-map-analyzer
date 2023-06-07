67fb73e3cfcdf1ab2b52634a60237c18f1aea23b
TomoyaFujita2016 <fujita.t.2016c@gmail.com>
2023-06-07 21:30:47 +0900

    limit

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ st.title("SNS検索")
 
 keyword = st.text_input("キーワードを入力してください", "スイーツ")
 location = st.text_input("場所を入力してください", value="東京駅")
-radius = st.slider("検索半径を選択してください(m)", min_value=3, max_value=10000, value=1000)
+radius = st.slider("検索半径を選択してください(m)", min_value=1, max_value=10000, value=1000)
 shop_type = st.selectbox(
     "タイプを選択してください", list(PlaceType), index=list(PlaceType).index(PlaceType.CAFE)
 )
@@ -31,7 +31,7 @@ search_col, download_col = st.columns([1, 1])  # set up the columns
 
 
 if search_col.button("検索"):
-    results = run_search_api(keyword, location, radius, limit=1, _type=shop_type)
+    results = run_search_api(keyword, location, radius, limit=5, _type=shop_type)
 
     # Prepare results for DataFrame
     formatted_results = []
